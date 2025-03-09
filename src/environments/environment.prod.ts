1808ec0e6b329fa04c0d19f23609d7e4f80902fa
DiegoTeodoro <diegoteodoro_alves@yahoo.com.br>
2025-03-09 11:10:28 -0300

    Alterando layout

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,11 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://172.31.109.129:3000',
+  apiUrl: 'http://192.168.100.110:3000',
+
+  dbConfig: {
+  host: "localhost",
+  user: "root",
+  password: "1234",
+  database: "igreja_db",
+  }
 };
